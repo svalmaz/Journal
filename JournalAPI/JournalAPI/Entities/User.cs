c21a0440bcc4ad8dfd4ac518b8173d2c10dff9bf
Almaz Saparbaev <almaz.0.saparbaev@gmail.com>
2024-12-03 00:15:53 +0600

    add entries

---
+++ JournalAPI/JournalAPI/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JournalAPI.Entities
+{
+	public class User
+	{
+		[Key]
+		public int Id { get; set; }
+		public string userName { get; set; }
+		public string userPass { get; set; }
+		public string userEmail { get; set; }
+	}
+	public class Entry
+	{
+		[Key]
+		public int Id { get; set; }
+		public int userId { get; set; }
+		public string text { get; set; }
+	}
+	public class Option
+	{
+		[Key]
+		public int Id { get; set; }
+		public int entriesId { get; set; }
+		public string options { get; set; }
+
+	}
+}
